fc3a72f29b5ab8ff14e2c8977a47ca5542c9997f
Alessandra <146783328+Leca20056@users.noreply.github.com>
2023-10-26 02:14:37 -0300

    Add files via upload
    Novo 26/10 as 02:15

--- js/script.js
+++ js/script.js
@@ -1,30 +1,39 @@
-// três constantes para referenciar as "ITEM" divs Projetos
-
+// tres constantes para referenciar as "ITEM" divs Projetos
 const album = document.getElementById('albumFotos');
-const casamentos = document.getElementById('casamentos');
+
+const casamento = document.getElementById('casamento');
+
 const buffet = document.getElementById('buffet');
+
 const salao = document.getElementById('salao');
 
+
 const movieAlbum = document.querySelector('.movieAlbum');
-const movieCasamentos = document.querySelector('.movieCasamentos');
-const movieBufett = document.querySelector('.movieBuffet');
+
+const movieCasamento = document.querySelector('.movieCasamento');
+
+const movieBuffet = document.querySelector('.movieBuffet');
+
 const movieSalao = document.querySelector('.movieSalao');
 
 
-// eveto de clik
-album.addEventListener('click', () =>{
-   album.classList.toggle('active');
-   movieAlbum.classList.toggle('active');
+
+
+
+// evento de click
+album.addEventListener('click',() =>{
+    album.classList.toggle('active');
+    movieAlbum.classList.toggle('active');
 });
-casamentos.addEventListener('click', () =>{
-    casamentos.classList.toggle('active');
-    movieCasamentos.classList.toggle('active');
-    });
-buffet.addEventListener('click' ,() =>{
-     buffet.classList.toggle('active');
-     movieBufett.classList.toggle('active');
-    });
+casamento.addEventListener('click',() =>{
+    casamento.classList.toggle('active');
+    movieCasamento.classList.toggle('active');
+});
+buffet.addEventListener('click',() =>{
+    buffet.classList('active');
+    movieBuffet.classList.toggle('active');
+});
 salao.addEventListener('click',() =>{
-    salao.classList.toggle('active');
+    salao.classList('active');
     movieSalao.classList.toggle('active');
 });
