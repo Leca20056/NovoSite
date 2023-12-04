579e27e9ce5679d01ffa6047bdd8b3d5cba03e39
Alessandra <146783328+Leca20056@users.noreply.github.com>
2023-12-04 18:47:36 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -167,9 +167,9 @@
         itemSelector: '.portfolio-item'
       });
 
-      let portfolioFilters = select('#portfolio-flters li', true);
+      let portfolioFilters = select('#portfolio-filters li', true);
 
-      on('click', '#portfolio-flters li', function(e) {
+      on('click', '#portfolio-filters li', function(e) {
         e.preventDefault();
         portfolioFilters.forEach(function(el) {
           el.classList.remove('filter-active');
